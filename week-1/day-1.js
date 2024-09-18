453fc3fa23e21f69530de6e199c2645615718c2f
John <emailjohnthomascaballero@gmail.com>
2024-09-18 12:06:56 +0800

    Day 1

---
+++ week-1/day-1.js
@@ -0,0 +1,18 @@
+// TODO: Demonstrate number type
+const age1 = 30;
+
+// TODO: Demonstrate string type
+const firstName1 = "John";
+
+// TODO: Demonstrate boolean type
+const isAdmin1 = true;
+
+// TODO: Demonstrate array type
+const numbers1 = [1, 2, 3, 4, 5];
+
+// TODO: Demonstrate tuple type
+const myTuple1 = [10, "Hello", true, ["John", "Jane"]];
+console.log(myTuple1[3][1]);
+
+// TODO: Demonstrate any type
+const anything1 = "John";
---
+++ week-1/day-1.ts
@@ -0,0 +1,19 @@
+// TODO: Demonstrate number type
+const age: number = 30;
+
+// TODO: Demonstrate string type
+const firstName: string = "John";
+
+// TODO: Demonstrate boolean type
+const isAdmin: boolean = true;
+
+// TODO: Demonstrate array type
+const numbers: number[] = [1, 2, 3, 4, 5];
+const names: string[] = ["John", "Jane", "Joe"];
+
+// TODO: Demonstrate tuple type
+const myTuple: [number, string, boolean, string[]] = [10, "Hello", true, ["John", "Jane"]];
+
+// TODO: Demonstrate any type
+const anything: any = "John";
+
