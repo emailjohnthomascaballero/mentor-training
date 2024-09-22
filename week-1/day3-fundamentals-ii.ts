408385375a50a017caf5de632847a840076dfc72
John <emailjohnthomascaballero@gmail.com>
2024-09-22 19:07:59 +0800

    Day 5

--- week-1/day3-fundamentals-ii.ts
+++ week-1/day3-fundamentals-ii.ts
@@ -2,8 +2,39 @@
 
 // TODO: Create a class with inheritance
 
-class Dog {
-  constructor (public name: string){
+
+//  class only:
+// class Dog {
+//   constructor (public name: string){
+//     this.name = name;
+//   }
+
+//   getName(){
+//     return this.name;
+//   }
+
+//   setName(name: string){
+//     this.name = name;
+//   }
+// }
+
+// const dog1 = new Dog("Fido");
+
+// dog1.getName();
+// dog1.setName("Rex");
+
+
+//  with interface:
+// this is mirror only and you can use it in testing
+interface DogInterface {
+  name: string;
+  getName(): string;
+  setName(name: string): void;
+}
+
+class Dog implements DogInterface {
+  public name;
+  constructor (name: string){
     this.name = name;
   }
 
@@ -20,4 +51,34 @@ const dog1 = new Dog("Fido");
 
 dog1.getName();
 dog1.setName("Rex");
-dog1.getName();
+
+// Mock is used for testing.
+class MockDog implements DogInterface {
+  public name;
+  constructor (name: string){
+    this.name = name;
+  }
+
+  getName(){
+    return this.name;
+  }
+
+  setName(name: string){
+    this.name = name;
+  }
+}
+
+
+// TODO: Demonstrate generics (important if you are working in complex projects)
+
+
+// example 1:
+type GenericDog<TProperties> = {
+  name: string;
+  properties: TProperties;
+}
+
+const dog3: GenericDog<number> = {
+  name: "Fido",
+  properties: 10
+}
