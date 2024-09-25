72d16ac8210b007a8cbf349936f0fdae4a1feb30
John <emailjohnthomascaballero@gmail.com>
2024-09-25 19:41:25 +0800

    Day 8

--- week-1/day3-fundamentals-ii.ts
+++ week-1/day3-fundamentals-ii.ts
@@ -197,13 +197,69 @@ const planet1 = Planets.Mercury;
 
 
 // example 3
-type GetPlanetDataArgs<TData> = {
-  id: string;
-  data: TData;
+type User = {
+  name: string;
+  age: number;
 }
 
-function getPlanetData<TData>({ id, data }: GetPlanetDataArgs<TData>) {
-  return { id, data };
+type Admin = {
+  name: string;
+  age: number;
+  adminAccess: boolean;
 }
 
-const planetData = getPlanetData<number>({id: '1', data: 123}); 
+type GetUserProperties<TType extends "user" | "admin"> = TType extends "user"
+  ? User
+  : Admin;
+
+const userData: GetUserProperties<"user"> = {
+  name: "John",
+  age: 20,
+};
+
+const adminData: GetUserProperties<"admin"> = {
+  name: "John",
+  age: 20,
+  adminAccess: true,
+};
+
+// not recommend to use this arrow function
+// const getUserProperties1 = <TType extends "user" | "admin">(userType: TType) => {
+//   return {} as GetUserProperties<TType>;
+// };
+
+// use this function instead.
+function getUserProperties <TType extends "user" | "admin">(userType: TType) {
+  return {} as GetUserProperties<TType>;
+};
+
+const userData2 = getUserProperties("user");
+userData2.name;
+userData2.age;
+
+const adminData2 = getUserProperties("admin");
+adminData2.name;
+adminData2.age;
+adminData2.adminAccess;
+
+
+
+
+
+// EXAMPLES OF BAD PRACTICES
+
+// not recommend to use arrow function
+// const aaaa = {
+//   name: "John",
+//   test: () => {
+//     // this.name; // Cannot access name here
+//   },
+// };
+
+// use function instead
+// const bbbb = {
+//   name: "John",
+//   test: function () {
+//     this.name;
+//   },
+// };
