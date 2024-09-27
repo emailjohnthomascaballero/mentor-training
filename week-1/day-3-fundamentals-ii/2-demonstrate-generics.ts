b145f74988d22e722860f592d40c54fa5fa6bd50
John <emailjohnthomascaballero@gmail.com>
2024-09-27 18:10:41 +0800

    Day 10

--- week-1/day-3-fundamentals-ii/2-demonstrate-generics.ts
+++ week-1/day-3-fundamentals-ii/2-demonstrate-generics.ts
@@ -34,29 +34,31 @@
 // console.log(dog4.properties.age);
 
 // example 3 (set default parameters):
-type GenericDog<TProperties = Record<string, unknown> > = { // set default value
-  name: string;
-  properties: TProperties;
-}
-
-const dog4: GenericDog = { // no need parameters
-  name: "Fido",
-  properties: { name: "Fido", age: 10, color: "black" }
-}
-
-// example 4 (extends will add more strict properties):
-// type GenericDog2<TProperties extends Record<string, unknown> > = { //instead of =
+// type GenericDog<TProperties = Record<string, unknown> > = { // set default value
 //   name: string;
 //   properties: TProperties;
 // }
 
-// type DogProperties = { // it adds strict here
-//   name: string;
-//   age: number;
-//   color: string;
-// }
-
-// const dog4: GenericDog2<DogProperties> = { 
+// const dog4: GenericDog = { // no need parameters
 //   name: "Fido",
 //   properties: { name: "Fido", age: 10, color: "black" }
 // }
+
+// console.log(dog4.properties.color);
+
+// example 4 (extends will add more strict properties):
+type GenericDog2<TProperties extends Record<string, unknown> > = { //instead of =
+  name: string;
+  properties: TProperties;
+}
+
+type DogProperties = { // it adds strict here
+  name: string;
+  age: number;
+  color: string;
+}
+
+const dog4: GenericDog2<DogProperties> = { 
+  name: "Fido",
+  properties: { name: "Fido", age: 10, color: "black" }
+}
