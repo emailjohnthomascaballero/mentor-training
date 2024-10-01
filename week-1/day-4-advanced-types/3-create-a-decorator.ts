a3208d910ad14f127001a21fceab28f8d8313d9c
John <emailjohnthomascaballero@gmail.com>
2024-10-01 20:53:45 +0800

    Day 14

--- week-1/day-4-advanced-types/3-create-a-decorator.ts
+++ week-1/day-4-advanced-types/3-create-a-decorator.ts
@@ -1,1 +1,26 @@
 // TODO: Create a decorator
+function log(
+  target: any,
+  methodKeyName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  descriptor.value = function (...args: any[]) {
+    console.log(`Calling ${methodKeyName} with arguments: ${args}`);
+    const result = originalMethod.apply(this, args);
+    console.log(`Method ${methodKeyName} returned: ${result}`);
+    return result;
+  };
+  return descriptor;
+}
+
+class PersonSomething {
+  @log
+  greet(name: string) {
+    console.log("This is calling from the greet method");
+    return `Hello, ${name}`;
+  }
+}
+
+const personSomething = new PersonSomething();
+personSomething.greet("John");
