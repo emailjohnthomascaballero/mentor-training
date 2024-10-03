96f593c18f4daeb051c336fa8e0125ec3859ba2c
John <emailjohnthomascaballero@gmail.com>
2024-10-03 12:30:35 +0800

    Day 16

--- week-1/day-4-advanced-types/4-utility-types.ts
+++ week-1/day-4-advanced-types/4-utility-types.ts
@@ -1,1 +1,8 @@
 // TODO: Use utility types (Partial, Pick, Omit, Readonly, Record, Required, NonNullable, ReturnType, Await
+
+type BaseUser = {
+  name: string;
+  age: number;
+};
+
+type OptionalUser = Partial<BaseUser>;
