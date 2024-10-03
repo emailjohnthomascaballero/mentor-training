745a230a88c9016f450fb863d8861d893c9aad10
John <emailjohnthomascaballero@gmail.com>
2024-10-03 19:37:46 +0800

    Day 16

--- week-1/day-4-advanced-types/4-utility-types.ts
+++ week-1/day-4-advanced-types/4-utility-types.ts
@@ -3,6 +3,38 @@
 type BaseUser = {
   name: string;
   age: number;
+  address: string;
 };
 
 type OptionalUser = Partial<BaseUser>;
+type PickUser = Pick<BaseUser, "name" | "address">;
+type OmitUser = Omit<BaseUser, "address">;
+type ReadonlyUser = Readonly<BaseUser>;
+
+const OptionalUser: OptionalUser = {
+  name: "John",
+  age: undefined,
+};
+
+const PickUser: PickUser = {
+  name: "John",
+  address: "New York",
+};
+
+const OmitUser: OmitUser = {
+  name: "John",
+  age: 30,
+}
+
+// you cannot assign new value because it is readonly
+const ReadonlyUser: ReadonlyUser = {
+  name: "John",
+  age: 30,
+  address: "New York",
+};
+
+// ReadonlyUser.name = "Jane"; // this is an error
+
+console.log(OptionalUser.age);
+console.log(PickUser);
+console.log(OmitUser);
