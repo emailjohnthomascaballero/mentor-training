57174fda32db48ca0f91604d3180d200693f7f1f
John <emailjohnthomascaballero@gmail.com>
2024-10-08 18:22:13 +0800

    Day 21

--- week-1/day-4-advanced-types/5-mapped-type.ts
+++ week-1/day-4-advanced-types/5-mapped-type.ts
@@ -1,1 +1,20 @@
 // TODO: Create a mapped type
+type BaseUser2 = {
+  name: string;
+  age: number;
+  address: string;
+};
+
+type MappedType<TObj> = {
+  [K in keyof TObj & string as `${K}-test`]: TObj[K];
+};
+
+type test3 = MappedType<BaseUser2>;
+
+const test4: test3 = {
+  "name-test": "John",
+  "age-test": 20,
+  "address-test": "john@doe.com",
+};
+
+console.log(test4);
